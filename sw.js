b6320d2d4463e58cb9f7ed2c5101d2907549e4e1
rexram987-create <rexram987@gmail.com>
2026-09-21 13:49:59 +0300

    Cache Abdallah audio for offline PWA playback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C="arabic-names-v13";
-const A=["/","/index.html","/style.css","/app.js","/names.json","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
+const C="arabic-names-v14";
+const A=["/","/index.html","/style.css","/app.js","/audio/abdallah.mp3","/names.json","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("arabic-names-")&&k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
