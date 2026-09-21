bd80b2ecb686daaf466b0330914f0dedff35cffa
rexram987-create <rexram987@gmail.com>
2026-09-21 11:15:58 +0300

    Refresh PWA cache for segmented Abdallah pronunciation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="arabic-names-v12";
+const C="arabic-names-v13";
 const A=["/","/index.html","/style.css","/app.js","/names.json","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("arabic-names-")&&k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
