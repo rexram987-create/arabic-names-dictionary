e2f5339b9c702b6967e64405c5c0bf82ff2fc9ea
rexram987-create <rexram987@gmail.com>
2026-09-21 15:24:45 +0300

    Cache Abd original WAV in PWA v16

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C="arabic-names-v15";
-const A=["/","/index.html","/style.css","/app.js","/audio/Generated%20Audio%20September%2021%2C%202026%20-%201_41PM.wav","/names.json","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
+const C="arabic-names-v16";
+const A=["/","/index.html","/style.css","/app.js","/audio/Generated%20Audio%20September%2021%2C%202026%20-%201_41PM.wav","/audio/Generated%20Audio%20September%2021%2C%202026%20-%202_54PM.wav","/names.json","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("arabic-names-")&&k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
