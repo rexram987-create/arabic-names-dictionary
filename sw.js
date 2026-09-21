ccf733fe792bbdcb75a7362f63d5e3c94c04384f
rexram987-create <rexram987@gmail.com>
2026-09-21 09:54:52 +0300

    Refresh installed PWA cache for Lubna search fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="arabic-names-v9";
+const C="arabic-names-v10";
 const A=["/","/index.html","/style.css","/app.js","/names.json","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("arabic-names-")&&k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
